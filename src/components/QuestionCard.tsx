fbc8a77d5cbfb33d82fca8e56c57fa71cf643b61
phildunn81 <phillipdunn@hotmail.com>
2020-10-07 13:39:30 +0100

    logic added for the quiz and api hooked up

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./QuestionCard.module.scss";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+}) => (
+  <div>
+    <p className={styles.questionNumber}>
+      Question: {questionNumber}/{totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }}></p>
+    <div>
+      {answers.map((answer) => (
+        <div key={answer}>
+          <button disabled={userAnswer} value={answer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+export default QuestionCard;
