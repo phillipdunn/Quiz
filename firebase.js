3627432be458a46c9cf9e9a45d5f74d1015f56f3
phildunn81 <phillipdunn@hotmail.com>
2020-10-09 09:55:20 +0100

    deployed

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/app";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDR_1DZp2vwyBfPyJRwyhWetgRjeN8-gx4",
+    authDomain: "quiz-f664a.firebaseapp.com",
+    databaseURL: "https://quiz-f664a.firebaseio.com",
+    projectId: "quiz-f664a",
+    storageBucket: "quiz-f664a.appspot.com",
+    messagingSenderId: "1064528578468",
+    appId: "1:1064528578468:web:5b9158e6f5f4a254f32331",
+    measurementId: "G-V61JKBQK4P"
+};
+
+export default firebase;
